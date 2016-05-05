f423ae84fbf3b976aa3c50e1b866adf35b56ad07
danrnascimento <danrnascimento@gmail.com>
2016-05-05 00:02:10 -0300

    Acertando o preenchimento

--- Matriz Ponteiro.c
+++ Matriz Ponteiro.c
@@ -14,22 +14,19 @@ int main(){
     printf("Digite agora as colunas: ");
     scanf("%d", &colunas);
 
+    system("clear");
+
 	float **matriz = alocarMatrizDinamica(linhas,colunas);
-	
+
+	system("clear");
+
+    printf("\n\tSUA MATRIZ: \n\n");
+
 	for(i = 0; i < linhas; i++){
 		for(j = 0; j < colunas; j++){
-			printf("Digite o valor da posicao[ %d ] [ %d ]: ", i, j);
-			scanf("%f", &matriz[i][j]);
+			printf("\t[ %2.f ]", *(*(matriz + i) + j));
 		}
-	}
-	
-	system("cls");
-	
-	for(i = 0; i < linhas; i++){
-		for(j = 0; j < colunas; j++){
-			printf("\t[ %d ]", *(*(matriz + i) + j));
-		}
-		printf("\n");
+		printf("\n\n");
 	}
 
     return 0;
@@ -37,14 +34,22 @@ int main(){
 
 
 float **alocarMatrizDinamica(int linha, int coluna){
-	
+
     float **matriz = (float**) malloc(linha * sizeof(float *));
-    int i;
+    int i,j;
 
     for (i = 0; i < linha; i++){
         *(matriz + i) = (float*) malloc(coluna * sizeof(float));
     }
-    
+
+    for(i = 0; i < linha; i++){
+		for(j = 0; j < coluna; j++){
+			printf("Digite o valor da posicao[ %d ] [ %d ]: ", i, j);
+			scanf("%f", &(*(*(matriz + i) + j)));
+		}
+		printf("\n");
+	}
+
     return matriz;
 }
 
